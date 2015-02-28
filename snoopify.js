d22e7f16a23f6a0965309dd28657475d26852a45
Omkar Moghe <omoghe@umich.edu>
2015-02-28 10:19:35 -0600

    Reorganized repo

--- snoopify.js
+++ snoopify.js
@@ -3,18 +3,18 @@ chrome.storage.sync.get("snoop", function(data) {
     snoop = data.snoop;
 });
 
-var snoop_square = "http://i.imgur.com/HVex5Ec.gif";
-var snoop_slim = "http://i.imgur.com/jcIK7fy.gif";
+if (snoop == 1) {
+    var snoop_square = "http://i.imgur.com/HVex5Ec.gif";
+    var snoop_slim = "http://i.imgur.com/jcIK7fy.gif";
 
-setTimeout(function(){
-    snoopify();
-}, 3420);
+    setTimeout(function(){
+        snoopify();
+    }, 3420);
 
-function snoopify () {
-    if (snoop == 1) {
-        var images = document.getElementsByTagName("img");
-        for (var i = 0; i < images.length; ++i) {
-            images[i].src = snoop_square;
-        }
+    function snoopify () {
+            var images = document.getElementsByTagName("img");
+            for (var i = 0; i < images.length; ++i) {
+                images[i].src = snoop_square;
+            }
     }
 }
