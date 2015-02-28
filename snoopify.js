8bb5049e6e636c34dcbd06c5d411c3754b3d2381
Omkar Moghe <omoghe@umich.edu>
2015-02-28 12:38:05 -0600

    Fixed fix fml.

--- snoopify.js
+++ snoopify.js
@@ -3,18 +3,18 @@ chrome.storage.sync.get("snoop", function(data) {
     snoop = data.snoop;
 });
 
-if (snoop == 1) {
-    var snoop_square = "http://i.imgur.com/HVex5Ec.gif";
-    var snoop_slim = "http://i.imgur.com/jcIK7fy.gif";
+var snoop_square = "http://i.imgur.com/HVex5Ec.gif";
+var snoop_slim = "http://i.imgur.com/jcIK7fy.gif";
 
-    setTimeout(function(){
+setTimeout(function(){
+    if (snoop == 1) {
         snoopify();
-    }, 3420);
-
-    function snoopify () {
-            var images = document.getElementsByTagName("img");
-            for (var i = 0; i < images.length; ++i) {
-                images[i].src = snoop_square;
-            }
     }
+}, 3420);
+
+function snoopify () {
+        var images = document.getElementsByTagName("img");
+        for (var i = 0; i < images.length; ++i) {
+            images[i].src = snoop_square;
+        }
 }
